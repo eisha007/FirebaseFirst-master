d655faf1864ed84b15c84fc03090bae5eda83a96
ep007 <eisha.peyyeti@gmail.com>
2020-03-08 23:09:26 -0500

    Initial commit

---
+++ app/src/main/java/com/codingblocks/firebasefirst/Note.java
@@ -0,0 +1,33 @@
+package com.codingblocks.firebasefirst;
+import android.content.Context;
+
+
+
+public class Note {
+    String title;
+    String subtitle;
+
+    public Note() {
+    }
+
+    public Note(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+}
